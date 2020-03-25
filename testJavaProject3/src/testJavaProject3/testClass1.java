61d2cc33845a9271c5f44d5af39fe9f0f27e4ee2
Ritika Mehta <ritzm03@gmail.com>
2020-03-25 20:12:29 +0530

    Commiting

---
+++ testJavaProject3/src/testJavaProject3/testClass1.java
@@ -0,0 +1,10 @@
+package testJavaProject3;
+
+public class testClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey! I am testClass1");
+	}
+
+}
