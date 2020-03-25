548768b6c9d35647f0792d02e313809727c11ffc
ritika034 <44552570+ritika034@users.noreply.github.com>
2020-03-25 20:45:34 +0530

    Update testClass1.java

--- testJavaProject3/src/testJavaProject3/testClass1.java
+++ testJavaProject3/src/testJavaProject3/testClass1.java
@@ -5,6 +5,7 @@ public class testClass1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hey! I am testClass1");
+		System.out.println("Hey! I am testClass1 edit on github");
 	}
 
 }
